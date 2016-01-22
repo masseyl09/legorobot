6fe0faad7549eb32a3f64ec06fcd718ef1af20f3
Jess Updegrove <jessupde@gmail.com>
2016-01-21 21:44:54 -0500

    Minor cleanup

--- src/legorobot/test.java
+++ src/legorobot/test.java
@@ -6,15 +6,12 @@ import lejos.utility.Delay;
 
 // Basic test code found at:
 // http://thetechnicgear.com/2014/04/howto-install-lejos-0-8-1-lego-mindstorms-ev3/screenshot-6/
-// Testing!
 
 public class test {
 	
 	public static void main(String[] args) {
-		GraphicsLCD g = BrickFinder.getDefault().getGraphicsLCD();
-		
+		GraphicsLCD g = BrickFinder.getDefault().getGraphicsLCD();	
 		g.drawString("Hello World", 0, 0, GraphicsLCD.VCENTER | GraphicsLCD.LEFT);
-		
 		Delay.msDelay(5000);
 	}
 }
