fc8074067a66ecb8ee8c2b942ec3a00c364ed790
Lauren Massey <masseyl09@gmail.com>
2016-03-04 18:30:49 -0500

    Adding speak

--- src/legorobot/Speak.java
+++ src/legorobot/Speak.java
@@ -1,13 +1,18 @@
 package legorobot;
 
+import java.util.Locale;
+
 import javax.speech.Central;
 import javax.speech.synthesis.Synthesizer;
+import javax.speech.synthesis.SynthesizerModeDesc;
 /**
  * @author Jess
  * @version 1.0
  * @created 06-Feb-2016 3:10:09 PM
  */
 public class Speak {
+
+	
 
 
 	public Speak(){
@@ -19,6 +24,16 @@ public class Speak {
 	 * @param response
 	 */
 	public void respond(String response){
+	try{
+		Synthesizer leaTalk = Central.createSynthesizer(new SynthesizerModeDesc(null, "general", Locale.US, null, null));
+		leaTalk.speak("Hello", null);
+		
+	} catch (Exception e){
+		e.printStackTrace();
+	}
+	
+	
+		
 
 	}
 
